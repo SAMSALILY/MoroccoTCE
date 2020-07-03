d1e919bf91cdb78cbe4b9306b3b33d54b1f335fe
RODI <rodi-jamal@hotmail.com>
2020-07-03 23:36:50 +0100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,186 @@
+// document.getElementById("submit-btn").addEventListener("click", function(e){
+//   event.preventDefault();
+ 
+
+ 
+//   if (document.getElementById("purpose").value=="Select Flight purpose"){ return alert("Select Flight purpose");}
+
+//   if (document.getElementById("business").selected=true){ window.open("https://www.2m.ma")}
+//   if (document.getElementById("commercial").selected=true){ window.open("https://starsoverflight.com")}
+//   if (document.getElementById("cargo").selected=true){ window.open("https://starsoverflight.com")}
+//   if (document.getElementById("fuelstop").selected=true){ window.open("https://starsoverflight.com")}
+// });
+
+
+// var u=true
+// document.getElementById("submit-btn-data").addEventListener("click", function(e){
+
+// //  e.preventDefault();
+ 
+ 
+//   if (document.getElementById("airport").value=="Select Airport"){ return alert("Select Airport");}
+ 
+  
+//   if ((document.getElementById("gmmn").selected=true) &&( u==true)){ var my=window.open("https://www.wish.com", "_blank"); u=false }
+ 
+  
+//   if ((document.getElementById("gmmx").selected=true) &&( u==true)){ my.close(); window.location.replace("https://storenvy.com", "_blank"); u=false}
+
+//   if ((document.getElementById("gmad").selected=true) && (u==true)){my.close(); window.location.replace("https://etsy.com", "_blank"); u=false}
+
+//   if ((document.getElementById("gmme").selected=true) && (u==true)){my.close(); window.location.replace("https://www.32m.ma", "_blank"); u=false}
+
+//    u=false
+//   if (document.getElementById("select-airport").selected=true){}
+
+
+// })
+
+document.getElementById("home").addEventListener("click", function(){
+  document.getElementById("gmmn-data").style.display="none"
+  document.getElementById("gmad-data").style.display="none"
+  document.getElementById("gmag-data").style.display="none"
+  document.getElementById("gmat-data").style.display="none"
+  document.getElementById("gmaz-data").style.display="none"
+  document.getElementById("gmfb-data").style.display="none"
+  document.getElementById("gmff-data").style.display="none"
+  document.getElementById("gmfi-data").style.display="none"
+  document.getElementById("gmfk-data").style.display="none"
+  document.getElementById("gmfo-data").style.display="none"
+  document.getElementById("gmmb-data").style.display="none"
+  document.getElementById("gmmd-data").style.display="none"
+  document.getElementById("gmme-data").style.display="none"
+  document.getElementById("gmmh-data").style.display="none"
+
+  document.getElementById("gmmi-data").style.display="none"
+  document.getElementById("gmml-data").style.display="none"
+  document.getElementById("gmmt-data").style.display="none"
+  document.getElementById("gmmw-data").style.display="none"
+  document.getElementById("gmmx-data").style.display="none"
+  document.getElementById("gmmz-data").style.display="none"
+  document.getElementById("gmta-data").style.display="none"
+  document.getElementById("gmtn-data").style.display="none"
+  document.getElementById("gmtt-data").style.display="none"
+ // event.preventDefault();
+})
+
+document.getElementById("gmmn").addEventListener("click", function(){  
+  
+ //  
+  document.getElementById("gmmn-data").style.display=""
+ // event.preventDefault();
+})
+
+  document.getElementById("gmad").addEventListener("click", function(){
+
+  
+ document.getElementById("gmad-data").style.display=""
+ // event.preventDefault();
+})
+
+document.getElementById("gmag").addEventListener("click", function(){
+   
+  document.getElementById("gmag-data").style.display=""
+  //event.preventDefault();
+})
+document.getElementById("gmat").addEventListener("click", function(){
+   
+  document.getElementById("gmat-data").style.display=""
+  //event.preventDefault();
+})
+
+document.getElementById("gmaz").addEventListener("click", function(){
+   
+  document.getElementById("gmaz-data").style.display=""
+  //event.preventDefault();
+})
+document.getElementById("gmfb").addEventListener("click", function(){
+   
+  document.getElementById("gmfb-data").style.display=""
+  //event.preventDefault();
+})
+document.getElementById("gmff").addEventListener("click", function(){
+   
+  document.getElementById("gmff-data").style.display=""
+  //event.preventDefault();
+})
+document.getElementById("gmfi").addEventListener("click", function(){
+   
+  document.getElementById("gmfi-data").style.display=""
+  //event.preventDefault();
+})
+document.getElementById("gmfk").addEventListener("click", function(){
+   
+  document.getElementById("gmfk-data").style.display=""
+  //event.preventDefault();
+})
+document.getElementById("gmfo").addEventListener("click", function(){
+   
+  document.getElementById("gmfo-data").style.display=""
+  //event.preventDefault();
+})
+document.getElementById("gmmb").addEventListener("click", function(){
+   
+  document.getElementById("gmmb-data").style.display=""
+  //event.preventDefault();
+})
+document.getElementById("gmmd").addEventListener("click", function(){
+   
+  document.getElementById("gmmd-data").style.display=""
+  //event.preventDefault();
+})
+document.getElementById("gmme").addEventListener("click", function(){
+   
+  document.getElementById("gmme-data").style.display=""
+  //event.preventDefault();
+})
+document.getElementById("gmmh").addEventListener("click", function(){
+   
+  document.getElementById("gmmh-data").style.display=""
+  //event.preventDefault();
+})
+document.getElementById("gmmi").addEventListener("click", function(){
+   
+  document.getElementById("gmmi-data").style.display=""
+  //event.preventDefault();
+})
+document.getElementById("gmml").addEventListener("click", function(){
+   
+  document.getElementById("gmml-data").style.display=""
+  //event.preventDefault();
+})
+document.getElementById("gmmt").addEventListener("click", function(){
+   
+  document.getElementById("gmmt-data").style.display=""
+  //event.preventDefault();
+})
+document.getElementById("gmmw").addEventListener("click", function(){
+   
+  document.getElementById("gmmw-data").style.display=""
+  //event.preventDefault();
+})
+document.getElementById("gmmx").addEventListener("click", function(){
+   
+  document.getElementById("gmmx-data").style.display=""
+  //event.preventDefault();
+})
+document.getElementById("gmmz").addEventListener("click", function(){
+   
+  document.getElementById("gmmz-data").style.display=""
+  //event.preventDefault();
+})
+document.getElementById("gmta").addEventListener("click", function(){
+   
+  document.getElementById("gmta-data").style.display=""
+  //event.preventDefault();
+})
+document.getElementById("gmtn").addEventListener("click", function(){
+   
+  document.getElementById("gmtn-data").style.display=""
+  //event.preventDefault();
+})
+document.getElementById("gmtt").addEventListener("click", function(){
+   
+  document.getElementById("gmtt-data").style.display=""
+  //event.preventDefault();
+})
